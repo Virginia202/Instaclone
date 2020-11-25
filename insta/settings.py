0064415dd6e0f41c8e9214a819a3855946d85ad6
Virginia202 <virgisonie20@gmail.com>
2020-11-25 12:32:05 +0300

    deploy to heroku

--- insta/settings.py
+++ insta/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from decouple import config,Csv
 import dj_database_url
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
